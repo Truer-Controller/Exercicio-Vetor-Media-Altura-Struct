7097bccbe57271c8a20c1593222e22e61b3950ab
Rafael Augusto <rafajunior2907@gmail.com>
2022-02-07 22:40:13 -0300

    Add project files.

---
+++ exerciciovetor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace exerciciovetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberChoose = int.Parse(Console.ReadLine());
+            double[] heightPeaplo = new double[numberChoose];
+
+            for (int i = 0; i < numberChoose; i++)
+            {
+                heightPeaplo[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);    
+            }
+
+            double sum = 0.0;
+            for(int i = 0; i < numberChoose; i++)
+            {
+                 sum += heightPeaplo[i];
+            }
+
+            double avaregeHeight = sum / numberChoose;
+
+            Console.WriteLine("Avarege Height = "+ avaregeHeight.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
